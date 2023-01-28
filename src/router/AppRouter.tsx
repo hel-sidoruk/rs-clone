2b630fbeef289f69b234a4ac320c4bbe079372ac
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-01-28 12:57:04 +0300

    fix: add leaderboard page and route

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Home, Challenges, Kata, KataTrain, UserProfile } from '../pages';
+import { Leaderboard } from '../pages/Leaderboard';
 
 export default function AppRouter() {
   return (
@@ -11,6 +12,7 @@ export default function AppRouter() {
       <Route path="/kata/:id" element={<Kata />} />
       <Route path="/kata/:id/train" element={<KataTrain />} />
       <Route path="/users/:id" element={<UserProfile />} />
+      <Route path="/users/leaderbord" element={<Leaderboard />} />
     </Routes>
   );
 }
