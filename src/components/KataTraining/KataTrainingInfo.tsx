14ed62848531cbf181643a378899f2eee74f4f48
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-01-29 11:49:07 +0300

    feat: add buttons in kata training info component

--- src/components/KataTraining/KataTrainingInfo.tsx
+++ src/components/KataTraining/KataTrainingInfo.tsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const options = ['Instructions', 'Output', 'Past Solutions'];
 
 export const KataTrainingInfo = () => {
-  return <div className="kata-train__descr">KataTrainingInfo</div>;
+  const [active, setActive] = useState(options[0]);
+
+  const getClassname = (i: number) => {
+    return `controls__btn ${active === options[i] ? 'active' : ''}`;
+  };
+
+  return (
+    <div className="kata-train__info">
+      <div className="controls">
+        <button className={getClassname(0)} onClick={() => setActive(options[0])}>
+          Instructions
+        </button>
+        <button className={getClassname(1)} onClick={() => setActive(options[1])}>
+          Output
+        </button>
+        <button className={getClassname(2)} onClick={() => setActive(options[2])}>
+          Past Solutions
+        </button>
+      </div>
+      <div className="kata-train__descr"></div>
+    </div>
+  );
 };
