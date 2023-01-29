77327e97d407be7a09e25aa61a1888289ca39dcb
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-01-29 23:32:25 +0300

    feat: add kata details

--- src/pages/Kata.tsx
+++ src/pages/Kata.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { KataControls } from '../components/Kata/KataControls';
 import { KataDescription } from '../components/Kata/KataDescription';
+import { KataInstructions } from '../components/KataTraining/KataInstructions';
 
 export const Kata = () => {
   return (
@@ -9,6 +10,10 @@ export const Kata = () => {
       <h1 className="page-title">Kata</h1>
       <KataDescription />
       <KataControls />
+      <div className="section kata-details">
+        <h3 className="kata-details__title">Description:</h3>
+        <KataInstructions />
+      </div>
       <Outlet />
     </main>
   );
