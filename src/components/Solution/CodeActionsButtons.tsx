c8421ed7e4de5adaa42eb0fe859008113bfed0af
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-01-29 10:04:44 +0300

    feat: add icons

--- src/components/Solution/CodeActionsButtons.tsx
+++ src/components/Solution/CodeActionsButtons.tsx
@@ -6,13 +6,16 @@ export const CodeActionsButtons = () => {
     <div className="code__btns">
       <div className="code__btns-actions">
         <Link to="/kata/2/train" className="btn btn-dark">
-          skip
+          <i className="icon-moon icon-moon-next"></i>
+          <span className="btn-text">skip</span>
         </Link>
         <Link to="/kata/1/solutions" className="btn btn-dark">
-          view solutions
+          <i className="icon-moon icon-moon-compare"></i>
+          <span className="btn-text">view solutions</span>
         </Link>
         <Link to="/kata/1/discuss" className="btn btn-dark">
-          discuss
+          <i className="icon-moon icon-moon-comments"></i>
+          <span className="btn-text">discuss</span>
         </Link>
         <button className="btn btn-dark">reset</button>
       </div>
