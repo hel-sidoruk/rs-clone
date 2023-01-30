58b007683a100990b5a1c9ff576cedc6b401807f
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-01-30 14:01:02 +0300

    feat: add content

--- src/components/Home/HeaderHome.tsx
+++ src/components/Home/HeaderHome.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const HeaderHome = () => {
+  const [isAuth, setIsAuth] = useState(true);
+
   return (
     <div className="header-home">
       <img src="/codewars.svg" alt="Codewars logo" />
       <div className="navbar_menu-buttons">
-        <Link to="/users/login" className="header-home__btn">
-          Log in
-        </Link>
-        <Link to="/users/registration" className="header-home__btn red">
-          Join
-        </Link>
+        {isAuth ? (
+          <>
+            <Link to="/kata" className="header-home__btn red">
+              Enter the Dojo
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to="/users/login" className="header-home__btn">
+              Log in
+            </Link>
+            <Link to="/users/registration" className="header-home__btn red">
+              Join
+            </Link>
+          </>
+        )}
       </div>
     </div>
   );
