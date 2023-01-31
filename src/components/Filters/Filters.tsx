e55e0f664e8aad380cf817b9722a875978735fb6
Alexandra Samoilova <avsamoilava@gmail.com>
2023-01-31 18:53:17 +0300

    feta: add basic render for filters component

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
+import { FilterDropDown } from './FilterDropDown';
+import SearchIcon from '../Icons/SearchIcon';
+const list = ['Newest', 'Oldest', 'Popularity', 'Hardest', 'Easiest', 'Name'];
 
 const Filters = () => {
   return (
     <div className="library__filters filters">
-      <div className="filters__search">search</div>
-      <div className="filters__drop-down sort">Sort By</div>
-      <div className="filters__drop-down status">Status</div>
-      <div className="filters__drop-down progress">Progress</div>
-      <div className="filters__drop-down difficulty">Difficulty</div>
-      <div className="filters__drop-down tags-filter">Tags</div>
+      <div className="filters__search">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+      <div className="filters__drop-down sort">
+        Sort By
+        <FilterDropDown list={list} />
+      </div>
+      <div className="filters__drop-down status">
+        Status
+        <FilterDropDown list={list} />
+      </div>
+      <div className="filters__drop-down progress">
+        Progress
+        <FilterDropDown list={list} />
+      </div>
+      <div className="filters__drop-down difficulty">
+        Difficulty
+        <FilterDropDown list={list} />
+      </div>
+      <div className="filters__drop-down tags-filter">
+        Tags
+        <FilterDropDown list={list} />
+      </div>
     </div>
   );
 };
