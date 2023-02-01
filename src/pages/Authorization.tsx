d3bbf781cec48799b7e9bd3bb538f837c33b4902
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-02-01 15:49:36 +0300

    feat: add auth routes

--- src/pages/Authorization.tsx
+++ src/pages/Authorization.tsx
@@ -1,24 +1,17 @@
 import React, { useEffect } from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { AuthComponent } from '../components/Authorization/AuthComponent';
 import useActions from '../hooks/useActions';
-import useTypedSelector from '../hooks/useTypedSelector';
 
 export const Authorization = ({ option }: { option: 'login' | 'registration' }) => {
   const [params] = useSearchParams();
-  const navigate = useNavigate();
   const { githubLogin, githubRegistration } = useActions();
-  const { isAuthorized } = useTypedSelector((state) => state.authorizedUser);
 
   useEffect(() => {
     const { code } = Object.fromEntries(params.entries());
     if (!code) return;
     option === 'login' ? githubLogin(code) : githubRegistration(code);
   }, [params]);
-
-  useEffect(() => {
-    isAuthorized && navigate('/');
-  }, [isAuthorized]);
 
   return (
     <div className="auth">
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,31 +1,42 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import useTypedSelector from '../hooks/useTypedSelector';
 import {
-  Home,
-  KataLibrary,
-  Kata,
-  KataTraining,
-  UserProfile,
   Authorization,
+  Home,
+  Kata,
   KataDiscuss,
+  KataLibrary,
   KataSolutions,
+  KataTraining,
   Leaderboard,
+  UserProfile,
 } from '../pages';
 
 export default function AppRouter() {
+  const { isAuthorized } = useTypedSelector((state) => state.authorizedUser);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/kata" element={<KataLibrary />} />
-      <Route path="/kata/:id" element={<Kata />}>
-        <Route path="/kata/:id/discuss" element={<KataDiscuss />} />
-        <Route path="/kata/:id/solutions" element={<KataSolutions />} />
-      </Route>
-      <Route path="/kata/:id/train" element={<KataTraining />} />
-      <Route path="/login" element={<Authorization option="login" />} />
-      <Route path="/registration" element={<Authorization option="registration" />} />
-      <Route path="/users/:id" element={<UserProfile />} />
-      <Route path="/users/leaderboard" element={<Leaderboard />} />
+      {isAuthorized ? (
+        <>
+          <Route path="/kata" element={<KataLibrary />} />
+          <Route path="/kata/:id" element={<Kata />}>
+            <Route path="/kata/:id/discuss" element={<KataDiscuss />} />
+            <Route path="/kata/:id/solutions" element={<KataSolutions />} />
+          </Route>
+          <Route path="/kata/:id/train" element={<KataTraining />} />
+          <Route path="/users/:id" element={<UserProfile />} />
+          <Route path="/users/leaderboard" element={<Leaderboard />} />
+        </>
+      ) : (
+        <>
+          <Route path="/login" element={<Authorization option="login" />} />
+          <Route path="/registration" element={<Authorization option="registration" />} />
+        </>
+      )}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
