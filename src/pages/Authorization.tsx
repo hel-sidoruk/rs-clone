d40f92ac03d61617b4fe47571e646a10c5be4fbf
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-02-01 17:04:34 +0300

    feat: add loader in authorization page

--- src/pages/Authorization.tsx
+++ src/pages/Authorization.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { AuthComponent } from '../components/Authorization/AuthComponent';
+import Loader from '../components/UI/Loader';
 import useActions from '../hooks/useActions';
+import useTypedSelector from '../hooks/useTypedSelector';
 
 export const Authorization = ({ option }: { option: 'login' | 'registration' }) => {
   const [params] = useSearchParams();
   const { githubLogin, githubRegistration } = useActions();
+  const { loading } = useTypedSelector((state) => state.authorizedUser);
 
   useEffect(() => {
     const { code } = Object.fromEntries(params.entries());
@@ -15,6 +18,7 @@ export const Authorization = ({ option }: { option: 'login' | 'registration' }) 
 
   return (
     <div className="auth">
+      {loading && <Loader />}
       <AuthComponent option={option} />
     </div>
   );
