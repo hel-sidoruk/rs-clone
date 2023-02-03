1b09d226911b2a4d02d2a5bbf367d501cd0380bd
Alexandra Samoilova <avsamoilava@gmail.com>
2023-02-04 00:40:31 +0300

    feat: add leaders reducer, action

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import authUserReducer from './authUserReducer';
 import kataReducer from './kataReducer';
+import leaderReducer from './leadersReducer';
 import { solutionReducer } from './solutionReducer';
 
 export const rootReducer = combineReducers({
   katas: kataReducer,
   authorizedUser: authUserReducer,
   solution: solutionReducer,
+  leaders: leaderReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
