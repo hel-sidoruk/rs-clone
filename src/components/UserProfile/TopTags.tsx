4e8d70d93365dcbb3d92c49e06a5e27e2edfa879
Alexandra Samoilova <avsamoilava@gmail.com>
2023-02-04 21:04:38 +0300

    feat: create TopTags component (hardcode)

---
+++ src/components/UserProfile/TopTags.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TopTags = () => {
+  return (
+    <div className="stats__top-tags">
+      <div className="stats__subtitle">
+        <i className="icon-moon-tag icon-moon colored"></i>
+        Top Tags:
+      </div>
+      <div>Fundamentals:112</div>
+      <div>Mathematics:47</div>
+      <div>Algorithms:37</div>
+      <div>Strings:30</div>
+      <div>Puzzles:9</div>
+      <div>Arrays:6</div>
+    </div>
+  );
+};
+export default TopTags;
