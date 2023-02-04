fc714aecafaba0be1ffe9a2371358acf1d7d83a1
Alexandra Samoilova <avsamoilava@gmail.com>
2023-02-04 21:20:38 +0300

    feat: add ProgressValue component (hardcode)

---
+++ src/components/UserProfile/ProgressValue.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ProgressValue = () => {
+  return (
+    <div className="stats__progress-value">
+      <div>
+        <span>Rank:</span>1 kyu
+      </div>
+      <div>
+        <span>Honor:</span>419,676
+      </div>
+      <div>
+        <span>Leaderboard Position:</span>#1
+      </div>
+      <div>
+        <span>Honor Percentile:</span>Top 0.000%
+      </div>
+      <div>
+        <span>Total Completed Kata:</span>2,644
+      </div>
+    </div>
+  );
+};
+export default ProgressValue;
