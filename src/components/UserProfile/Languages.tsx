842d748e78272e4a02e07c380db45ad7a90ac82a
Alexandra Samoilova <avsamoilava@gmail.com>
2023-02-05 23:02:07 +0300

    feat: create languages component (hardcode)

---
+++ src/components/UserProfile/Languages.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Languages = () => {
+  return (
+    <div className="stats__langs">
+      <div className="stats__subtitle">
+        <i className="icon-moon-terminal icon-moon colored"></i>
+        Languages:
+      </div>
+      <div>
+        <b>Total Languages Trained:</b>51
+      </div>
+      <div>
+        <b>Highest Trained:</b>JavaScript (1 kyu)
+      </div>
+      <div>
+        <b>Most Recent:</b>Groovy
+      </div>
+    </div>
+  );
+};
+export default Languages;
