8cb9226f51eba0826f7a223e2a63096d881660ce
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-02-08 20:24:07 +0300

    feat: add solutions hidden element

--- src/pages/KataSolutions.tsx
+++ src/pages/KataSolutions.tsx
@@ -18,8 +18,21 @@ export const KataSolutions = () => {
     <div className="section solution-main">
       <LeftBarForSolutions sol />
       <div>
-        {solutions.length &&
-          solutions.map((item) => <KataSolutionItem solution={item} key={nanoid()} />)}
+        {solutions.length ? (
+          solutions.map((item) => <KataSolutionItem solution={item} key={nanoid()} />)
+        ) : (
+          <div className="section locked-solutions">
+            <h3 className="locked-solutions__title">Solutions have been withheld</h3>
+            <p className="locked-solutions__text">
+              Since you have not yet solved this kata we have hidden the solutions from you. If you
+              choose to view the solutions you will forfeit your eligibility to earn honor/rank
+              progress for this kata.
+            </p>
+            <button className="btn btn-fill">
+              <i className="icon-moon icon-moon-unlock"></i>Unlock Solutions (forfeit eligibility)
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
