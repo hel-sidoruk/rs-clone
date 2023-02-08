28a31d543c7814661e5f384136d0b6e4034e3d12
Alexandra Samoilova <avsamoilava@gmail.com>
2023-02-08 19:22:57 +0300

    feat: add filterList const

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,3 +5,33 @@ export const WS_URL = 'ws://localhost:9000';
 export const SUITE_START = '--suite-start--';
 export const SUITE_END = '--suite-end--';
 export const DEFAULT_AVATAR = 'https://avatars.githubusercontent.com/u/6301773?s=100';
+
+export const filterLists = {
+  sort: ['Newest', 'Oldest', 'Popularity', 'Hardest', 'Easiest', 'Name'],
+  difficulty: ['8 kyu', '7 kyu', '6 kyu'],
+  progress: ['All', 'Not trained', 'Not completed', 'Completed'],
+  tags: [
+    'Algorithms',
+    'Arrays',
+    'Binary',
+    'Bits',
+    'Data Structures',
+    'Date Time',
+    'Debugging',
+    'Functional Programming',
+    'Fundamentals',
+    'Games',
+    'Geometry',
+    'Lists',
+    'Logic',
+    'Mathematics',
+    'Object-oriented Programming',
+    'Parsing',
+    'Puzzles',
+    'Recursion',
+    'Refactoring',
+    'Regular Expressions',
+    'Sorting',
+    'Strings',
+  ],
+};
