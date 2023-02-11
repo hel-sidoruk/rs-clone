6fe83d36af864c1424700b2fd8981fe747bc1ae0
Alexandra Samoilova <avsamoilava@gmail.com>
2023-02-12 00:18:56 +0300

    fix: cut description of articles

--- src/utils/articles.ts
+++ src/utils/articles.ts
@@ -15,9 +15,8 @@ export const articleList: ArticleLink[] = [
     url: 'https://www.codewars.com/post/10-traits-employers-look-for-when-hiring-software-engineers',
   },
   {
-    title: 'Would You Pass the Google SQL Interview? Test Yourself with These 7 Concepts',
-    description:
-      'How do you prepare for your Google SQL interview? You start by appreciating SQL as a query language, and SQL server as a relational database management system. ',
+    title: 'Would You Pass the Google SQL Interview?',
+    description: 'How do you prepare for your Google SQL interview? ',
     previewImg:
       'https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/62f25d989aa17f8019013442_62ebf4ab86fb653b308a54b6_AdobeStock_93299638-p-2000.jpeg',
     url: 'https://www.codewars.com/post/would-you-pass-the-google-sql-interview-test-yourself-with-these-7-concepts',
@@ -31,8 +30,7 @@ export const articleList: ArticleLink[] = [
   },
   {
     title: 'Codewars: Practice coding with active learning',
-    description:
-      'Learning new subject matter is hard. Learning code is even harder. The traditional way of learning tells us that if we pay attention in lectures, take good notes, and study hard, we can absorb just about anything. That’s not always the case. ',
+    description: 'Learning new subject matter is hard.',
     previewImg:
       'https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/62f25d9740a2c330263e0919_626824a50e15e04a3134e8ae_developer%2520learning.png',
     url: 'https://www.codewars.com/post/codewars-practice-coding-with-active-learning',
@@ -40,7 +38,7 @@ export const articleList: ArticleLink[] = [
   {
     title: 'How To Get Hired as a Software Developer During a Tech Recession',
     description:
-      'The startup fastidiously sources the latest layoff statistics while providing resources for recruiters looking to hire recently laid off talent.',
+      'The startup fastidiously sources the latest layoff statistics while providing resources...',
     previewImg:
       'https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/63e65afa4f702148c82d1103_AdobeStock_278511769-p-2000.jpeg',
     url: 'https://www.codewars.com/post/how-to-get-hired-as-a-software-developer-during-a-tech-recession',
@@ -48,47 +46,42 @@ export const articleList: ArticleLink[] = [
   {
     title:
       'Learn in Public: The community-based learning strategy that improves programming skills',
-    description:
-      'Raise your hand if you’re guilty of lurking on the internet. We all do it. There’s no shame in it. However, if lurking is your main avenue of learning, you’re doing yourself a disservice.',
+    description: 'Raise your hand if you’re guilty of lurking on the internet.',
     previewImg:
       'https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/634970c7e46422910a8d7a1a_630e1d8e57125cc85723f9aa_AdobeStock_287272918-p-2000.jpeg',
     url: 'https://www.codewars.com/post/learn-in-public-the-community-based-learning-strategy-that-improves-programming-skills',
   },
   {
     title: 'What is Mastery Learning, and How Does It Apply to Coding?',
-    description:
-      'Everyone reading this article has personal experience with education — at some point you learned how to read, how to multiply, how to write, and how to experiment. ',
+    description: 'Everyone reading this article has personal experience with education...',
     previewImg:
       'https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/63d182594cc2108f0c5a4e3a_Untitled%20design%20(1)-p-2000.png',
     url: 'https://www.codewars.com/post/what-is-mastery-learning-and-how-does-it-apply-to-coding',
   },
   {
     title: 'TypeScript and JavaScript: Their relationship, explained',
-    description: `Are you confused about the relationship between TypeScript and JavaScript? Are you wondering if TypeScript is just a fancier version of JavaScript? Or maybe you're curious about the pros and cons of using TypeScript and when it's best to use it?`,
+    description: `Are you confused about the relationship between TypeScript and JavaScript?`,
     previewImg:
       'https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/63c6ddf0ac599f7d7235ec6e_AdobeStock_457833532-p-2000.jpeg',
     url: 'https://www.codewars.com/post/typescript-and-javascript-the-relationship-explained',
   },
   {
     title: 'Developer Productivity: Strengthening flow through meditation',
-    description:
-      'What comes to mind when you hear the word "meditation"? Is it skepticism? Disregard? Belief? Peace of mind? A tool for success?',
+    description: 'What comes to mind when you hear the word "meditation"?',
     previewImg:
       'https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/63b72732c1807c8ce60d55f6_AdobeStock_354073031-p-1600.jpeg',
     url: 'https://www.codewars.com/post/developer-productivity-strengthening-flow-through-meditation',
   },
   {
     title: '8 Reasons Why Codewarriors Practice Coding with Codewars',
-    description:
-      'When thinking of Codewars, improving developer skills is top-of-mind. But there has to be more, right? We wanted to learn more about personal use and the nuanced ways users gain value from Codewars.',
+    description: 'When thinking of Codewars, improving developer skills is top-of-mind.',
     previewImg:
       'https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/63c96f663436595d79a62d43_codewars-og-image.png',
     url: 'https://www.codewars.com/post/8-reasons-why-codewarriors-practice-coding-with-codewars',
   },
   {
     title: 'How to Begin a New Career as a Software Engineer',
-    description:
-      'A Few Tips To Make Your Transition Into Programming Easier. Making a career change can be as simple as re-discovering what you want to be when you grow up. ',
+    description: 'A Few Tips To Make Your Transition Into Programming Easier.',
     previewImg:
       'https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/634972a36fe7a91ae0880f67_fernando-hernandez-vSI8ASKyMJ8-unsplash-full-size.png',
     url: 'https://www.codewars.com/post/how-to-begin-a-new-career-as-a-software-engineer',
@@ -96,36 +89,35 @@ export const articleList: ArticleLink[] = [
   {
     title: 'What is SQL? 8 resources for beginners',
     description:
-      'SQL stands for Structured Query Language, and it’s used for all things data-related. At its core, SQL is a language that lets you communicate with and manipulate data in databases. ',
+      'SQL stands for Structured Query Language, and it’s used for all things data-related.',
     previewImg:
       'https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/62f25d98530a76a5d3fbee0b_629e736140914eb53ba0da0c_sql-big.jpeg',
     url: 'https://www.codewars.com/post/what-is-sql-8-resources-for-beginners',
   },
   {
     title: 'Web3: Introducing the next iteration of the internet',
-    description:
-      'You’ve probably heard about Web3 by now. Blockchain technologies are getting a ton of hype and criticism.',
+    description: 'You’ve probably heard about Web3 by now.',
     previewImg:
       'https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/62f25d98eafac55fa4546b3f_629e9b90058c3a33da68819e_web3.jpeg',
     url: 'https://www.codewars.com/post/web3-introducing-the-next-iteration-of-the-internet',
   },
   {
     title: '10 Different Types of Software Engineers: A comprehensive guide',
-    description: `Software engineer' is catch all phrase for several career paths in tech that work with software. However, taking a closer look reveals that 'software engineer' doesn't begin to accurately describe the in and outs of the many software engineer roles available.`,
+    description: `Software engineer' is catch all phrase for several career paths in tech that work with software.`,
     previewImg:
       'https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/634970c7cbeed5837b11b938_62fd57ccd6890f25796f92f9_AdobeStock_295461823-p-2000.jpeg',
     url: 'https://www.codewars.com/post/10-different-types-of-software-engineers-a-comprehensive-guide',
   },
   {
     title: 'Quick Start Guide: Golang for Beginners',
-    description: `If you are new to coding, you may wonder if it's worth learning. Go's not the most in-demand language. It doesn't even break the top ten most used programming languages, according to Stack Overflow.`,
+    description: `If you are new to coding, you may wonder if it's worth learning.`,
     previewImg:
       'https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/635286fb76237656e97fe599_6350087f39cfe50ec46dcaf2_Golang%25202-p-2000.jpeg',
     url: 'https://www.codewars.com/post/quick-start-guide-golang-for-beginners',
   },
   {
     title: 'Top SQL Interview Questions and Answers in 2022',
-    description: `Structured Query Language or SQL is a powerful programming language that is used in many different technical fields and industries. If you are looking to land a career as a data analyst, data engineer, or data scientist then you'll need to learn and master SQL.`,
+    description: `Structured Query Language or SQL is a powerful programming language...`,
     previewImg:
       'https://uploads-ssl.webflow.com/62e95dddfb380a0e61193e7d/6362b44b8560665eefc874b6_AdobeStock_206292429-p-2000.jpeg',
     url: 'https://www.codewars.com/post/top-sql-interview-questions-and-answers-in-2022',
