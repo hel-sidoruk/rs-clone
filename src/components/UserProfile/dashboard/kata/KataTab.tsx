b393f4f1a5c0ad635d0d79e200e7addc26fa1db9
Alexandra Samoilova <avsamoilava@gmail.com>
2023-02-11 18:01:06 +0300

    feat: create KataTab component

--- src/components/UserProfile/dashboard/kata/KataTab.tsx
+++ src/components/UserProfile/dashboard/kata/KataTab.tsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const KataTab = () => {
-  return <h2>Kata-tab</h2>;
+  const [opened, setOpened] = useState('completed');
+  return (
+    <div className="dashboard-wrapper kata-tab">
+      <div className="dashboard-wrapper__sidebar">
+        <Link
+          to="/users/:id/completed"
+          className={opened === 'completed' ? '_opened' : ''}
+          onClick={() => setOpened('completed')}
+        >
+          Completed
+        </Link>
+        <Link
+          to="/users/:id/authored"
+          className={opened === 'authored' ? '_opened' : ''}
+          onClick={() => setOpened('authored')}
+        >
+          Authored
+        </Link>
+      </div>
+      <div className="dashboard-wrapper__content">kata-list</div>
+    </div>
+  );
 };
