7a15676f7125d91d95c998c826f713e2de6d6144
Alexandra Samoilova <avsamoilava@gmail.com>
2023-02-11 18:00:16 +0300

    feat: add handlers for tabs

--- src/components/UserProfile/UserStats.tsx
+++ src/components/UserProfile/UserStats.tsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import { StatsTab } from './dashboard';
 
 const UserStats = () => {
+  const [openedTab, setOpenedTab] = useState('stats');
+
   return (
     <div className="user-profile__stats user-stats">
       <div className="user-stats__tabs">
-        <Link to={'/users/:id/stats'} className="user-stats__tab user-stats__tab_open">
+        <Link
+          to={'/users/:id/stats'}
+          className={`user-stats__tab${openedTab === 'stats' ? ' _opened-tab' : ''}`}
+          onClick={() => setOpenedTab('stats')}
+        >
           Stats
         </Link>
-        <Link to={'/users/:id/completed'} className="user-stats__tab">
+        <Link
+          to={'/users/:id/completed'}
+          className={`user-stats__tab${openedTab === 'kata' ? ' _opened-tab' : ''}`}
+          onClick={() => setOpenedTab('kata')}
+        >
           Kata
         </Link>
-        <Link to={'/users/:id/collections'} className="user-stats__tab">
+        <Link
+          to={'/users/:id/collections'}
+          className={`user-stats__tab${openedTab === 'collections' ? ' _opened-tab' : ''}`}
+          onClick={() => setOpenedTab('collections')}
+        >
           Collections
         </Link>
-        <Link to={'/users/:id/comments'} className="user-stats__tab">
+        <Link
+          to={'/users/:id/comments'}
+          className={`user-stats__tab${openedTab === 'discourse' ? ' _opened-tab' : ''}`}
+          onClick={() => setOpenedTab('discourse')}
+        >
           Discourse
         </Link>
       </div>
