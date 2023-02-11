d62423a8fca98c2823b175dc8344ab4470b1a379
Alexandra Samoilova <avsamoilava@gmail.com>
2023-02-11 18:00:45 +0300

    feat: fill empty collections component

--- src/components/UserProfile/dashboard/collections/CollectionsTab.tsx
+++ src/components/UserProfile/dashboard/collections/CollectionsTab.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export const CollectionsTab = () => {
-  return <h2>Collections-Tab</h2>;
+  return (
+    <div className="dashboard-wrapper collections">
+      <div className="dashboard-wrapper__sidebar">Authored</div>
+      <div className="dashboard-wrapper__content" style={{ fontSize: 16 }}>
+        There are no items to show
+      </div>
+    </div>
+  );
 };
