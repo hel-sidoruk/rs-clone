62eb7616f2a665783480da017646c40e4641b1e8
Alexandra Samoilova <avsamoilava@gmail.com>
2023-02-11 23:10:35 +0300

    fix: correct path in links

--- src/components/UserProfile/UserStats.tsx
+++ src/components/UserProfile/UserStats.tsx
@@ -1,35 +1,36 @@
 import React, { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { UserInterface } from '../../types/user';
 
-const UserStats = () => {
+const UserStats = ({ user }: { user: UserInterface }) => {
   const [openedTab, setOpenedTab] = useState('stats');
 
   return (
     <div className="user-profile__stats user-stats">
       <div className="user-stats__tabs">
         <Link
-          to={'/users/:id/stats'}
+          to={`/users/${user.username}/stats`}
           className={`user-stats__tab${openedTab === 'stats' ? ' _opened-tab' : ''}`}
           onClick={() => setOpenedTab('stats')}
         >
           Stats
         </Link>
         <Link
-          to={'/users/:id/completed'}
+          to={`/users/${user.username}/completed`}
           className={`user-stats__tab${openedTab === 'kata' ? ' _opened-tab' : ''}`}
           onClick={() => setOpenedTab('kata')}
         >
           Kata
         </Link>
         <Link
-          to={'/users/:id/collections'}
+          to={`/users/${user.username}/collections`}
           className={`user-stats__tab${openedTab === 'collections' ? ' _opened-tab' : ''}`}
           onClick={() => setOpenedTab('collections')}
         >
           Collections
         </Link>
         <Link
-          to={'/users/:id/comments'}
+          to={`/users/${user.username}/comments`}
           className={`user-stats__tab${openedTab === 'discourse' ? ' _opened-tab' : ''}`}
           onClick={() => setOpenedTab('discourse')}
         >
