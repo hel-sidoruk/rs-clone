f594a177c32ce637502926a86d1892f858873bca
Alexandra Samoilova <avsamoilava@gmail.com>
2023-02-13 22:50:46 +0300

    fix: correct get next rank

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,5 @@ export * from './constants';
 export * from './getOutputLineClassname';
 export * from './articles';
 export * from './getRandomIndex';
-export * from './getNextRank';
 export * from './getPercentScore';
 export * from './rankColorSelector';
