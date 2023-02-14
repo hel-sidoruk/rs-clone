43dd74ba02108581f22702dc61762d7cbf9c217c
Alexandra Samoilova <avsamoilava@gmail.com>
2023-02-14 16:12:11 +0300

    feat: change api query on store

--- src/components/UserProfile/dashboard/solutions/SolutionsTab.tsx
+++ src/components/UserProfile/dashboard/solutions/SolutionsTab.tsx
@@ -1,29 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { UsersAPI } from '../../../../api';
-import { initialUser } from '../../../../pages';
+import { Link } from 'react-router-dom';
+import { SolutionsAPI } from '../../../../api/SolutionsAPI';
+import useTypedSelector from '../../../../hooks/useTypedSelector';
 
 export const SolutionsTab = () => {
   const [opened, setOpened] = useState('completed');
-  const [user, setUser] = useState(initialUser);
-  const { id } = useParams();
+  const { currentUser } = useTypedSelector((state) => state.user);
 
   useEffect(() => {
-    UsersAPI.getOne(id as string).then((data) => setUser(data));
-  }, []);
+    SolutionsAPI.getUserSolutions();
+  });
 
   return (
     <div className="dashboard-wrapper solutions-tab">
       <div className="dashboard-wrapper__sidebar">
         <Link
-          to={`/users/${user.username}/solutions`}
+          to={`/users/${currentUser?.username}/solutions`}
           className={opened === 'completed' ? '_opened' : ''}
           onClick={() => setOpened('completed')}
         >
           Completed
         </Link>
         <Link
-          to={`/users/${user.username}/solutions`}
+          to={`/users/${currentUser?.username}/solutions`}
           className={opened === 'unfinished' ? '_opened' : ''}
           onClick={() => setOpened('unfinished')}
         >
