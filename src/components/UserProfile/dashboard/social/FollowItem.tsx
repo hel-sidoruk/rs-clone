9cadac1e9171fb520faa25a289bacff5867590d8
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-02-15 15:04:52 +0300

    feat: add link to user page in socialTab

--- src/components/UserProfile/dashboard/social/FollowItem.tsx
+++ src/components/UserProfile/dashboard/social/FollowItem.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FollowerInterface } from '../../../../types/followers';
 import { FakeAvatar } from '../../../Icons';
 import { Rank } from '../../../Kata/Rank';
 
 const FollowItem = ({ user, isFollower }: { user: FollowerInterface; isFollower?: boolean }) => {
+  const username = isFollower ? user.username : user.followUser;
   return (
     <div className="follow-list__item follow-user">
       <Rank rank={user.rank} />
       <FakeAvatar />
-      <div className="follow-user__name">{isFollower ? user.username : user.followUser}</div>
+      <Link to={`/users/${username}/`} className="follow-user__name link">
+        {username}
+      </Link>
       <div className="follow-user__clan">
         {user.clan && <i title="Clan" className="icon-moon-clan icon-moon"></i>}
         {user.clan}
