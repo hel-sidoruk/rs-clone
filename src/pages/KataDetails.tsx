3eb0d149c7ea061a47dc633d67c9bf96e44184c4
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-02-16 16:58:53 +0300

    feat: add articles in kata details

--- src/pages/KataDetails.tsx
+++ src/pages/KataDetails.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useOutletContext } from 'react-router-dom';
+import Article from '../components/Article';
 import { KataStats } from '../components/Kata/KataStats';
 import { SimilarKatas } from '../components/Kata/SimilarKatas';
 import { KataInstructions } from '../components/KataTraining/KataInstructions';
@@ -8,13 +9,19 @@ import { KataInterface } from '../types/kata';
 export const KataDetails = () => {
   const { kata } = useOutletContext<{ kata: KataInterface }>();
   return (
-    <div>
-      <div className="section kata-details">
-        <h3 className="kata-details__title">Description:</h3>
-        {kata && <KataInstructions description={kata.description} tags={kata.tags} />}
+    <div className="kata-page">
+      <div className="kata-page__content">
+        <div className="section kata-details">
+          <h3 className="kata-details__title">Description:</h3>
+          {kata && <KataInstructions description={kata.description} tags={kata.tags} />}
+        </div>
+        <SimilarKatas data={kata} />
+        {kata && <KataStats data={kata} />}
       </div>
-      <SimilarKatas data={kata} />
-      {kata && <KataStats data={kata} />}
+      <div className="kata-page__articles">
+        <Article />
+        <Article />
+      </div>
     </div>
   );
 };
