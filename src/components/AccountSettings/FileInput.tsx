f8317c883d253edd89dcf4cea4b917a72b51d6c2
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-02-16 15:38:54 +0300

    feat: add drag and drop file upload

--- src/components/AccountSettings/FileInput.tsx
+++ src/components/AccountSettings/FileInput.tsx
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import useTypedSelector from '../../hooks/useTypedSelector';
 import { Avatar } from '../UI/Avatar';
 
-export const FileInput = ({ setImage }: { setImage: (f: File) => void }) => {
+export const FileInput = ({ setImage }: { setImage: (f: File | null) => void }) => {
   const { avatar } = useTypedSelector((state) => state.account);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [dragActive, setDragActive] = useState(false);
+  const [uploadedImage, setUploadedImage] = useState<{
+    src: string | ArrayBuffer;
+    name: string;
+  } | null>(null);
 
   const selectFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) setImage(e.target.files[0]);
+  };
+
+  const handleClick = () => {
+    inputRef.current?.click();
+  };
+
+  const handleDrag = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!uploadedImage) setDragActive(true);
+  };
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (uploadedImage) return;
+    setDragActive(false);
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      const file = e.dataTransfer.files[0];
+      setImage(file);
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        if (e.target && e.target.result)
+          setUploadedImage({ src: e.target.result, name: file.name });
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  const resetImage = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!uploadedImage) return;
+    e.stopPropagation();
+    setUploadedImage(null);
+    setImage(null);
   };
 
   return (
@@ -16,7 +55,33 @@ export const FileInput = ({ setImage }: { setImage: (f: File) => void }) => {
       </label>
       <div className="file-upload">
         <Avatar src={avatar || ''} size="65px" />
-        <input type="file" className="input" onChange={selectFile} id="avatar" accept="image/*" />
+        <input
+          ref={inputRef}
+          type="file"
+          className="input"
+          onChange={selectFile}
+          id="avatar"
+          accept="image/*"
+        />
+        <div
+          className={`file-upload__field ${dragActive ? 'active' : ''}`}
+          onDragEnter={handleDrag}
+          onDragLeave={() => setDragActive(false)}
+          onDragOver={handleDrag}
+          onDrop={handleDrop}
+          onClick={handleClick}
+        >
+          {!uploadedImage && 'Drag your file here or'}
+          <div className="btn" onClick={resetImage}>
+            {uploadedImage ? 'Reset' : 'Upload file'}
+          </div>
+          {uploadedImage && (
+            <>
+              <img className="file-upload__image" src={uploadedImage.src as string} />
+              <span>{uploadedImage.name}</span>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
