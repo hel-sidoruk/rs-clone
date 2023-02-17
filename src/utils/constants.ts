ae8a74d913b8b066a417e5d06e0b27fc7c0c18a7
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-02-17 20:31:56 +0300

    fix: fix websocket urL

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,7 @@ import { CommentLabel } from '../types/comments';
 export const GITHUB_AUTH_URL = 'https://github.com/login/oauth/authorize';
 export const CLIENT_ID_LOGIN = 'b67da247620e2e266ba4';
 export const CLIENT_ID_REGISTRATION = 'ff8172a4250438893a45';
-export const WS_URL = 'ws://rs-clone-server-qh63.onrender.com:9000';
+export const WS_URL = 'wss://rs-clone-server-qh63.onrender.com';
 export const SUITE_START = '--suite-start--';
 export const SUITE_END = '--suite-end--';
 export const DEFAULT_AVATAR = 'https://avatars.githubusercontent.com/u/6301773?s=100';
