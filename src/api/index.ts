90387358f45d97ae1447a367ee1992dc31b93fd5
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-02-17 17:36:05 +0300

    feat: change api link

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,8 +3,8 @@ export * from './KataAPI';
 export * from './UsersAPI';
 import axios, { InternalAxiosRequestConfig } from 'axios';
 
-export const API_URL = 'http://localhost:5000/api';
-export const AUTH_URL = 'http://localhost:5000/api/auth';
+export const API_URL = 'https://rs-clone-server-qh63.onrender.com/api';
+export const AUTH_URL = 'https://rs-clone-server-qh63.onrender.com/api/auth';
 
 export const host = axios.create({
   baseURL: API_URL,
