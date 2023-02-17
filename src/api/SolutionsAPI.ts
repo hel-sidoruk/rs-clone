dfedf01689a1e2aa437c1bcc6aad482e3fe86513
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-02-17 13:45:20 +0300

    refactor: refactor users and solutions api

--- src/api/SolutionsAPI.ts
+++ src/api/SolutionsAPI.ts
@@ -1,24 +1,16 @@
-import { API_URL } from '.';
+import { authHost, host } from '.';
 import { SolutionInterface } from '../types';
 
 export class SolutionsAPI {
   static async getSolutions(kataId: string, username?: string): Promise<SolutionInterface[]> {
-    const response = await fetch(
-      `${API_URL}/kata/${kataId}/solutions${username ? `?username=${username}` : ''}`
+    const { data } = await host.get(
+      `/kata/${kataId}/solutions${username ? `?username=${username}` : ''}`
     );
-    const data = await response.json();
     return data;
   }
 
   static async getUserSolutions(): Promise<{ solutions?: SolutionInterface[]; message?: string }> {
-    const savedToken = localStorage.getItem('token');
-    if (!savedToken) return { message: 'No token found' };
-    const result = await fetch(`${API_URL}/solutions`, {
-      headers: {
-        Authorization: `Bearer ${savedToken}`,
-      },
-    });
-    const data = await result.json();
+    const { data } = await authHost.get(`/solutions`);
     return { solutions: data };
   }
 
@@ -26,14 +18,7 @@ export class SolutionsAPI {
     kataId: string,
     solution: { username: string; solution: string; name: string; rank: string }
   ): Promise<SolutionInterface> {
-    const response = await fetch(`${API_URL}/kata/${kataId}/solutions`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(solution),
-    });
-    const data = await response.json();
+    const { data } = await host.post(`/kata/${kataId}/solutions`, solution);
     return data;
   }
 
@@ -43,14 +28,7 @@ export class SolutionsAPI {
     updates: { cleverVotes?: number; bestPracticesVotes?: number }
   ): Promise<{ status: string } | { error: string }> {
     if (!Object.keys(updates).length) return { error: 'No params to update' };
-    const response = await fetch(`${API_URL}/kata/${kataId}/solutions/${solutionId}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(updates),
-    });
-    const data = await response.json();
+    const { data } = await host.patch(`/kata/${kataId}/solutions/${solutionId}`, updates);
     return data;
   }
 }
--- src/api/UsersAPI.ts
+++ src/api/UsersAPI.ts
@@ -1,43 +1,29 @@
-import axios from 'axios';
-import { API_URL } from '.';
+import { host } from '.';
 import { UserInterface } from '../types/user';
+
+const URL = '/user';
 
 export class UsersAPI {
   static async getAll(): Promise<UserInterface[]> {
-    const response = await fetch(`${API_URL}/user`);
-    const data = await response.json();
+    const { data } = await host.get(URL);
     return data;
   }
 
   static async getOne(id: string): Promise<UserInterface> {
-    const { data } = await axios.get(`${API_URL}/user/${id}`);
+    const { data } = await host.get(`${URL}/${id}`);
     return data;
   }
 
   static async create(user: UserInterface): Promise<UserInterface> {
-    const response = await fetch(`${API_URL}/user/`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(user),
-    });
-    const data = await response.json();
+    const { data } = await host.post(`${URL}`, user);
     return data;
   }
 
   static async update(
     id: string,
-    data: Partial<Omit<UserInterface, 'id' | 'username'>>
+    updates: Partial<Omit<UserInterface, 'id' | 'username'>>
   ): Promise<{ status?: 'ok' }> {
-    const response = await fetch(`${API_URL}/user/${id}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-    const res = await response.json();
-    return { status: res.status };
+    const { data } = await host.patch(`${URL}/${id}`, updates);
+    return { status: data.status };
   }
 }
