9d28d0770907a84bdf14b7d9ddecd2c87e8facf0
@olga_sidoruk <olya.sidoruk98@gmail.com>
2023-02-27 20:03:13 +0300

    fix: fix followers info

--- src/components/UserProfile/dashboard/social/FollowItem.tsx
+++ src/components/UserProfile/dashboard/social/FollowItem.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { UsersAPI } from '../../../../api';
 import { FollowerInterface } from '../../../../types/followers';
+import { UserInterface } from '../../../../types/user';
 import { Rank } from '../../../Kata/Rank';
 import { Avatar } from '../../../UI/Avatar';
 
 const FollowItem = ({ user, isFollower }: { user: FollowerInterface; isFollower?: boolean }) => {
   const username = isFollower ? user.username : user.followUser;
   const avatar = isFollower ? user.followerAvatar : user.followingAvatar;
+  const [follower, setFollower] = useState<UserInterface | null>(null);
+
+  useEffect(() => {
+    if (isFollower) UsersAPI.getOne(user.username).then((res) => setFollower(res));
+  }, [isFollower, user.username]);
 
   return (
     <div className="follow-list__item follow-user">
@@ -17,9 +24,20 @@ const FollowItem = ({ user, isFollower }: { user: FollowerInterface; isFollower?
       </Link>
       <div className="follow-user__clan">
         {user.clan && <i title="Clan" className="icon-moon-clan icon-moon"></i>}
-        {user.clan}
+        {!isFollower ? (
+          user.clan
+        ) : follower ? (
+          <>
+            <i title="Clan" className="icon-moon-clan icon-moon"></i>
+            {follower.clan}
+          </>
+        ) : (
+          ''
+        )}
       </div>
-      <div className="follow-user__honor">{user.honor}</div>
+      <div className="follow-user__honor">
+        {!isFollower ? user.honor : follower ? follower.honor : ''}
+      </div>
     </div>
   );
 };
